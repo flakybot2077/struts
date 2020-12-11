70a1df44032dbcee0f5dc4f67788b120926ea150
jmz11 <loveshe1998@126.com>
2020-12-10 18:29:38 -0600

    use fixed NPE version of NonDex

--- nondex.sh
+++ nondex.sh
@@ -9,9 +9,9 @@ if [[ ! -z $nondextests ]]
 then
     printf "Running NonDex on tests:\n$nondextests\n"
     nondextests=$(echo $nondextests | tr -s '[:blank:]' ',')
-    git clone --depth 1 https://github.com/TestingResearchIllinois/NonDex
+    git clone --depth 1 --branch surefire-npe-error-fix https://github.com/mojilin/NonDex.git
     cd NonDex
-    mvn clean ; mvn install -DskipTests
+    mvn install -DskipTests
     cd ..
     mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn edu.illinois:nondex-maven-plugin:1.1.3-SNAPSHOT:nondex -DnondexSeed=$(shuf -i 0-65000 -n 1) -DnondexRuns=10 -DfailIfNoTests=false -Dtest=$nondextests
 fi  
